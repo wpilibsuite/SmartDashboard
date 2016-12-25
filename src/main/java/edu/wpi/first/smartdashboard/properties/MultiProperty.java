0996ff518fe1cf245ba2108f6ee8a3b28c6a8f48
Austin Shalit <austinshalit@gmail.com>
2016-12-24 23:51:12 -0800

    Fix the issues caused by changing MultiProperty (#56)
    * Fix the issues caused by changing MultiProperty
    
    * Fix CameraServer Viewer Saved Value
    
    * Fix CameraServer viewer saving

--- src/main/java/edu/wpi/first/smartdashboard/properties/MultiProperty.java
+++ src/main/java/edu/wpi/first/smartdashboard/properties/MultiProperty.java
@@ -50,7 +50,7 @@ public class MultiProperty extends Property {
 
   @Override
   protected Object transformValue(Object value) {
-    return value;
+    return values.get(value);
   }
 
   @Override
@@ -60,12 +60,12 @@ public class MultiProperty extends Property {
 
   @Override
   public String getSaveValue() {
-
     for (Map.Entry<String, Object> entry : values.entrySet()) {
       if (entry.getValue().equals(getValue())) {
         return entry.getKey();
       }
     }
-    return null;
+
+    return getSavedValue();
   }
 }
