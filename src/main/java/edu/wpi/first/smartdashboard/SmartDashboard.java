a27173603ec7de09cd4f575bdd04d94fbbc686bf
Thad House <ThadHouse@users.noreply.github.com>
2018-10-10 21:21:40 -0700

    Update to Java 11 and new gradle (#106)
    * Update to Java 11
    
    Disables plugin support for now, as the changes required in Java 11 are going to be behavior changes, and we need to discuss. But we need this ready for beta. #107 discusses this.

--- src/main/java/edu/wpi/first/smartdashboard/SmartDashboard.java
+++ src/main/java/edu/wpi/first/smartdashboard/SmartDashboard.java
@@ -1,6 +1,5 @@
 package edu.wpi.first.smartdashboard;
 
-import edu.wpi.first.smartdashboard.extensions.FileSniffer;
 import edu.wpi.first.smartdashboard.gui.DashboardFrame;
 import edu.wpi.first.smartdashboard.properties.IntegerProperty;
 import edu.wpi.first.smartdashboard.robot.Robot;
@@ -69,7 +68,9 @@ public class SmartDashboard {
         1000);
 
     // Search the filesystem for extensions (49%)
-    FileSniffer.findExtensions(monitor, 0, 490);
+    //FileSniffer.findExtensions(monitor, 0, 490);
+    // Extensions disabled until classloading can be fixed, see
+    // https://github.com/wpilibsuite/SmartDashboard/issues/107
 
     // Parse arguments
     ArgParser argParser = new ArgParser(args, true, true, new String[]{"ip"});
