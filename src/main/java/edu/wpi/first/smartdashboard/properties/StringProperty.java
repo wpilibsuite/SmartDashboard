f8abdf09655dd8c51bbfea0b90e4eec8ff500df0
Tyler Tian <tylertian123@gmail.com>
2019-10-26 12:23:24 -0400

    Escape special characters in save files (#124)
    This fixes the saving bug for not just MJPEG Viewers, but also labels
    etc.

--- src/main/java/edu/wpi/first/smartdashboard/properties/StringProperty.java
+++ src/main/java/edu/wpi/first/smartdashboard/properties/StringProperty.java
@@ -14,6 +14,21 @@ public class StringProperty extends TextInputProperty<String> {
   }
 
   @Override
+  public String getSaveValue() {
+    // In XML, the characters "&" and "<" are not valid in values by themselves
+    // Therefore here we replace these invalid characters by their escape sequences.
+    // Retrieve the save value from the superclass
+    String saveValue = super.getSaveValue();
+    // Replace &, <, >, ', and "
+    saveValue = saveValue.replace("&", "&amp;");
+    saveValue = saveValue.replace("<", "&lt;");
+    saveValue = saveValue.replace(">", "&gt;");
+    saveValue = saveValue.replace("\'", "&apos;");
+    saveValue = saveValue.replace("\"", "&quot;");
+    return saveValue;
+  }
+
+  @Override
   protected String transformValue(Object value) {
     return value.toString();
   }
