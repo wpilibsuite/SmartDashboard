39104b440ff8187b708990d4f04154aee660170e
Joe <sciencewhiz@users.noreply.github.com>
2023-01-07 11:38:02 -0800

    Remove PID Widget features only used by old PIDController

--- src/main/java/edu/wpi/first/smartdashboard/gui/elements/PIDEditor.java
+++ src/main/java/edu/wpi/first/smartdashboard/gui/elements/PIDEditor.java
@@ -18,24 +18,18 @@ public class PIDEditor extends AbstractTableWidget implements Controller {
 
   public static final DataType[] TYPES = {PIDType.get()};
 
-  private static final String[] pidTypes = {"Displacement", "Rate"}; // kDisplacement, kRate
-
   private final boolean editType;
 
   private NumberTableComboBox tBox;
   private NumberTableField pField;
   private NumberTableField iField;
   private NumberTableField dField;
-  private NumberTableField fField;
   private NumberTableField sField;
-  private BooleanTableCheckBox eBox;
   private JLabel tLabel;
   private JLabel pLabel;
   private JLabel iLabel;
   private JLabel dLabel;
-  private JLabel fLabel;
   private JLabel sLabel;
-  private JLabel eLabel;
 
   public PIDEditor() {
     this(true);
@@ -50,66 +44,36 @@ public class PIDEditor extends AbstractTableWidget implements Controller {
   public void init() {
     setLayout(new GridBagLayout());
 
-    if (editType) {
-      tLabel = new JLabel("PID type: ");
-    }
     pLabel = new JLabel("P:");
     iLabel = new JLabel("I:");
     dLabel = new JLabel("D:");
-    fLabel = new JLabel("F:");
     sLabel = new JLabel("Setpoint:");
-    eLabel = new JLabel("Enabled:");
-    if (editType) {
-      tLabel.setHorizontalAlignment(JLabel.RIGHT);
-    }
     pLabel.setHorizontalAlignment(JLabel.RIGHT);
     iLabel.setHorizontalAlignment(JLabel.RIGHT);
     dLabel.setHorizontalAlignment(JLabel.RIGHT);
-    fLabel.setHorizontalAlignment(JLabel.RIGHT);
     sLabel.setHorizontalAlignment(JLabel.RIGHT);
-    eLabel.setHorizontalAlignment(JLabel.RIGHT);
-    if (editType) {
-      tBox = new NumberTableComboBox<>("PID Type", pidTypes);
-    }
     pField = new NumberTableField("p");
     iField = new NumberTableField("i");
     dField = new NumberTableField("d");
-    fField = new NumberTableField("f");
     sField = new NumberTableField("setpoint");
-    eBox = new BooleanTableCheckBox("enabled");
 
     int columns = 10;
     pField.setColumns(columns);
     iField.setColumns(columns);
     dField.setColumns(columns);
-    fField.setColumns(columns);
     sField.setColumns(columns);
-
-    if (editType) {
-      System.out.println("tbox=" + tBox);
-      System.out.println("table=" + table);
-      tBox.addActionListener(e -> table.putNumber("PID Type", tBox.getSelectedIndex()));
-    }
 
     GridBagConstraints c = new GridBagConstraints();
 
 
     c.gridy = 1;
-    if (editType) {
-      add(tLabel, c);
-    }
-    c.gridy = 2;
     add(pLabel, c);
-    c.gridy = 3;
+    c.gridy = 2;
     add(iLabel, c);
-    c.gridy = 4;
+    c.gridy = 3;
     add(dLabel, c);
-    c.gridy = 5;
-    add(fLabel, c);
-    c.gridy = 6;
+    c.gridy = 4;
     add(sLabel, c);
-    c.gridy = 7;
-    add(eLabel, c);
 
     c.gridx = 1;
     c.weightx = 1.0;
@@ -119,21 +83,13 @@ public class PIDEditor extends AbstractTableWidget implements Controller {
     nameTag.setHorizontalAlignment(JLabel.LEFT);
     nameTag.setText(getFieldName());
     c.gridy = 1;
-    if (editType) {
-      add(tBox, c);
-    }
-    c.gridy = 2;
     add(pField, c);
-    c.gridy = 3;
+    c.gridy = 2;
     add(iField, c);
-    c.gridy = 4;
+    c.gridy = 3;
     add(dField, c);
-    c.gridy = 5;
-    add(fField, c);
-    c.gridy = 6;
+    c.gridy = 4;
     add(sField, c);
-    c.gridy = 7;
-    add(eBox, c);
 
     setMaximumSize(new Dimension(Integer.MAX_VALUE, getPreferredSize().height));
 
@@ -147,6 +103,5 @@ public class PIDEditor extends AbstractTableWidget implements Controller {
 
   @Override
   public void reset() {
-    eBox.setBindableValue(false);
   }
 }
