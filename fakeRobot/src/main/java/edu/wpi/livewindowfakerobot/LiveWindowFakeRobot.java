dba81af05cd3bd6a202682e69423e5e469e43315
sciencewhiz <sciencewhiz@users.noreply.github.com>
2023-06-19 23:38:55 -0700

    Fix FakeRobot by fixing initialization order (#175)
    Was crashing on start-up because ntcore wasn't loaded

--- fakeRobot/src/main/java/edu/wpi/livewindowfakerobot/LiveWindowFakeRobot.java
+++ fakeRobot/src/main/java/edu/wpi/livewindowfakerobot/LiveWindowFakeRobot.java
@@ -15,63 +15,70 @@ import java.util.TimerTask;
  * @author Sam
  */
 public class LiveWindowFakeRobot {
-    
-    private static final NetworkTable liveWindow = NetworkTable.getTable("LiveWindow");
-    
-    private static final ITable STATUS          = createTable(liveWindow, ".status", "LW Status"),
-            
-                                wrist           = createTable(liveWindow, "Wrist", "LW Subsystem"),
-                                wPotentiometer  = createTable(wrist, "Potentiometer", "Analog Input"),
-                                wVictor         = createTable(wrist, "Victor", "Speed Controller"), 
 
-                                elevator        = createTable(liveWindow, "Elevator", "PIDSubsystem"), 
-                                ePotentiometer  = createTable(elevator, "Potentiometer", "Analog Input"), 
-                                eVictor         = createTable(elevator, "Victor", "Speed Controller"),
-            
-                                testSys         = createTable(liveWindow, "TestSystem", "LW Subsystem"), 
-                                tComp           = createTable(testSys, "Compressor", "Compressor"), 
-                                tGearTooth      = createTable(testSys, "Gear Tooth Sensor", "Gear Tooth"), 
-                                tVictor         = createTable(testSys, "Victor", "Speed Controller"), 
-                                tPotentiometer  = createTable(testSys, "Potentiometer", "Analog Input"), 
-                                tRelay          = createTable(testSys, "Spike", "Relay"), 
-                                tDigitalOutput  = createTable(testSys, "Digital Output", "Digital Output"), 
-                                tGyro           = createTable(testSys, "Gyro", "LWGyro"), 
-                                tSolenoid       = createTable(testSys, "Solenoid", "Solenoid"), 
-                                tServo          = createTable(testSys, "Serov the Servo", "Servo"), 
-                                tAccel          = createTable(testSys, "Accelerometer", "Accelerometer"), 
-                                tEncoder1       = createTable(testSys, "Encoder 1", "Encoder"), 
-                                tUltra          = createTable(testSys, "Ultrasonic", "Ultrasonic"), 
-                                tCompass        = createTable(testSys, "Compass", "Compass"), 
-                                tSwitch         = createTable(testSys, "Limit Switch", "Digital Input"),
-            
-                                canSystem       = createTable(liveWindow, "CAN Subsystem", "LW Subsystem"),
-                                canJag          = createTable(canSystem, "CAN Jaguar", "CANSpeedController"),
-                                canTalon        = createTable(canSystem, "CAN Talon", "CANSpeedController");
-    
+    private static NetworkTable liveWindow;
+
+    private static ITable STATUS, wrist, wPotentiometer, wVictor, elevator, ePotentiometer,
+        eVictor, testSys, tComp, tGearTooth, tVictor, tPotentiometer, tRelay, tDigitalOutput,
+        tGyro, tSolenoid, tServo, tAccel, tEncoder1, tUltra, tCompass, tSwitch, canSystem,
+        canJag, canTalon;
+
     public static void main(String[] args) throws IOException {
         WPIUtilJNI.Helper.setExtractOnStaticLoad(false);
         NetworkTablesJNI.Helper.setExtractOnStaticLoad(false);
         CombinedRuntimeLoader.loadLibraries(LiveWindowFakeRobot.class, "wpiutiljni", "ntcorejni");
-        
+
+        liveWindow = NetworkTable.getTable("LiveWindow");
+
+        STATUS = createTable(liveWindow, ".status", "LW Status");
+
+        wrist = createTable(liveWindow, "Wrist", "LW Subsystem");
+        wPotentiometer = createTable(wrist, "Potentiometer", "Analog Input");
+        wVictor = createTable(wrist, "Victor", "Speed Controller");
+
+        elevator = createTable(liveWindow, "Elevator", "PIDSubsystem");
+        ePotentiometer = createTable(elevator, "Potentiometer", "Analog Input");
+        eVictor = createTable(elevator, "Victor", "Speed Controller");
+
+        testSys = createTable(liveWindow, "TestSystem", "LW Subsystem");
+        tComp = createTable(testSys, "Compressor", "Compressor");
+        tGearTooth = createTable(testSys, "Gear Tooth Sensor", "Gear Tooth");
+        tVictor = createTable(testSys, "Victor", "Speed Controller");
+        tPotentiometer = createTable(testSys, "Potentiometer", "Analog Input");
+        tRelay = createTable(testSys, "Spike", "Relay");
+        tDigitalOutput = createTable(testSys, "Digital Output", "Digital Output");
+        tGyro = createTable(testSys, "Gyro", "LWGyro");
+        tSolenoid = createTable(testSys, "Solenoid", "Solenoid");
+        tServo = createTable(testSys, "Serov the Servo", "Servo");
+        tAccel = createTable(testSys, "Accelerometer", "Accelerometer");
+        tEncoder1 = createTable(testSys, "Encoder 1", "Encoder");
+        tUltra = createTable(testSys, "Ultrasonic", "Ultrasonic");
+        tCompass = createTable(testSys, "Compass", "Compass");
+        tSwitch = createTable(testSys, "Limit Switch", "Digital Input");
+
+        canSystem = createTable(liveWindow, "CAN Subsystem", "LW Subsystem");
+        canJag = createTable(canSystem, "CAN Jaguar", "CANSpeedController");
+        canTalon = createTable(canSystem, "CAN Talon", "CANSpeedController");
+
         System.out.println();
-        
+
         STATUS.putBoolean("LW Enabled", true);
         STATUS.putString("Robot", "Testing");
         wPotentiometer.putNumber("Value", 2.6);
         ePotentiometer.putNumber("Value", -11.6872);
         tSwitch.putString("Value", "Off");
-        
+
         elevator.putNumber("p", 0.5);
         elevator.putNumber("i", 0.5);
         elevator.putNumber("d", 0.5);
         elevator.putNumber("f", 0.5);
         elevator.putNumber("setpoint", 0.5);
         elevator.putBoolean("enabled", false);
-        
+
         canJag.putString("Type", "CANJaguar");
         canTalon.putString("Type", "CANTalon");
-        
-        
+
+
         (new Timer()).schedule(
             new TimerTask(){
                 @Override
@@ -88,11 +95,11 @@ public class LiveWindowFakeRobot {
                     tCompass.putNumber("Value", Math.random());
                     tUltra.putNumber("Value", (Math.random()-.5) * 200);
                     tGearTooth.putNumber("Value", (int)(Math.random() * 100));
-                }}, 
+                }},
             0, 500);
-        
+
     }
-    
+
     private static ITable createTable(ITable parent, String name, String type) {
         ITable table = parent.getSubTable(name);
         System.out.println(table);
